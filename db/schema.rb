1d5748de25b4a79d449274ad4eaa2def36d1d61a
Victoria B <viktorija.bourassa@gmail.com>
2019-03-26 01:44:30 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190326013757) do
+ActiveRecord::Schema.define(version: 20190326014401) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20190326013757) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "artist_id"
+    t.integer  "genre_id"
   end
 
 end
